4a799a299b3cca17a85ae05c38d4b4d280c74ffa
Qawelesizwe <qawemlilo@gmail.com>
2013-04-03 15:22:01 +0200

    ...

--- com_saservice/site/views/category/tmpl/default.php
+++ com_saservice/site/views/category/tmpl/default.php
@@ -4,73 +4,15 @@ defined('_JEXEC') or die('Restricted access');
 
 $document = &JFactory::getDocument();
 $document->addStyleSheet(JURI::base() . 'components/com_saservice/asserts/css/bootstrap.min.css');
+
+$service = JRequest::getVar('service', 'Not Specified', 'get', 'string');
+$location = JRequest::getVar('location', 'Not Specified', 'get', 'string');
 ?>
 
 <div class="row-fluid">
-  <img src="<?php echo JURI::base() . 'components/com_saservice/asserts/img/webdesigners.png'; ?>" />
+  <h1><?php echo $service . ' in ' . $location; ?></h1>
 </div>
-<div class="row-fluid">
-    <ul class="thumbnails" style="padding-left: 0px;">
-      <li class="span3">
-        <div class="thumbnail">
-          <img src="<?php echo JURI::base() . 'components/com_saservice/asserts/img/logo.png'; ?>" alt="">
-        </div>
-      </li>
-      
-      <li class="span3">
-        <div class="thumbnail">
-          <img src="<?php echo JURI::base() . 'components/com_saservice/asserts/img/logo.png'; ?>" alt="">
-        </div>
-      </li>
-      
-      <li class="span3">
-        <div class="thumbnail">
-          <img src="<?php echo JURI::base() . 'components/com_saservice/asserts/img/logo.png'; ?>" alt="">
-        </div>
-      </li>
-      
-      <li class="span3">
-        <div class="thumbnail">
-          <img src="<?php echo JURI::base() . 'components/com_saservice/asserts/img/logo.png'; ?>" alt="">
-        </div>
-      </li>
-    </ul>
-    
+<div class="row-fluid" style="padding: 20px; text-align: center;">
+  <img src="<?php echo JURI::base() . 'components/com_saservice/asserts/img/page-under-construction.jpg'; ?>" alt="">
+</div>
 
-    <ul class="thumbnails" style="padding-left: 0px;">
-      <li class="span3">
-        <div class="thumbnail">
-          <img src="<?php echo JURI::base() . 'components/com_saservice/asserts/img/logo.png'; ?>" alt="">
-        </div>
-      </li>
-      
-      <li class="span3">
-        <div class="thumbnail">
-          <img src="<?php echo JURI::base() . 'components/com_saservice/asserts/img/logo.png'; ?>" alt="">
-        </div>
-      </li>
-      
-      <li class="span3">
-        <div class="thumbnail">
-          <img src="<?php echo JURI::base() . 'components/com_saservice/asserts/img/logo.png'; ?>" alt="">
-        </div>
-      </li>
-      
-      <li class="span3">
-        <div class="thumbnail">
-          <img src="<?php echo JURI::base() . 'components/com_saservice/asserts/img/logo.png'; ?>" alt="">
-        </div>
-      </li>
-    </ul>
-</div>
-<div class="pagination pagination-centered">
-  <ul style="padding-left: 0px;">
-    <li><a href="#">Prev</a></li>
-    <li class="disabled"><a href="#">1</a></li>
-    <li><a href="#">2</a></li>
-    <li><a href="#">3</a></li>
-    <li><a href="#">4</a></li>
-    <li><a href="#">5</a></li>
-    <li><a href="#">Next</a></li>
-  </ul>
-</div>
