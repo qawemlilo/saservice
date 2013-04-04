e7f1ed48c3ca5656ff2e17f5b6d30462e749c161
Qawelesizwe <qawemlilo@gmail.com>
2013-04-04 16:30:16 +0200

    ..

--- mod_ssnsearch/helper.php
+++ mod_ssnsearch/helper.php
@@ -4,10 +4,25 @@ defined('_JEXEC') or die('Direct Access to this location is not allowed.');
 class ModSsnsearchHelper
 {
     public function getCategories () {
+        $db =& JFactory::getDBO();
+        $query = "SELECT id, name FROM #__ss_categories";
+        $db->setQuery($query);
+        
+        $result = $db->loadObjectList();
+        
+        return $result;
     
     }
     
-    public function getAreas () {
-    
+    public function createArry ($list) {
+        $arr = '[';
+        
+        foreach ($list as $cat) {
+            $arr .= '"' .$cat->name . '",';
+        }
+        
+        $arr .= '"Other"]';
+        
+        return $arr;
     }
 }
--- mod_ssnsearch/mod_ssnsearch.php
+++ mod_ssnsearch/mod_ssnsearch.php
@@ -3,4 +3,9 @@ defined('_JEXEC') or die('Direct Access to this location is not allowed.');
 
 require_once(dirname(__FILE__).DS.'helper.php');
 
+
+$cats = ModSsnsearchHelper::getCategories();
+$arr = ModSsnsearchHelper::createArry($cats);
+
+
 require(JModuleHelper::getLayoutPath('mod_ssnsearch'));
